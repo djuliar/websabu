50cdd52a6fb4b4b5f30fe84c8565ebfd9e7a7d4a
David Juli Ariyadi <30861975+djuliar@users.noreply.github.com>
2026-05-05 13:31:41 +0800

    Implement Database class for MySQL connection
    Added Database class for database connection with error handling.

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,25 @@
 <?php
-echo "Belajar PHP Native";
+class Database {
+    // Konfigurasi database
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $db   = "db_kampus";
+   
+    // Properti untuk koneksi database
+    public $conn;
+    public function __construct() {
+        // Membuat koneksi ke database dengan error handling
+        try {
+            $this->conn = new mysqli(
+                $this->host, $this->user, $this->pass, $this->db
+            );
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+    public function getConnection() {
+        return $this->conn;
+    }
+}
 ?>
